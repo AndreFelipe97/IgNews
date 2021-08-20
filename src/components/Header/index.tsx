dc67e2bff50e5146c57c169e5919ebcfb2066fc7
AndreRodrigues97 <78046838+AndreRodrigues97@users.noreply.github.com>
2021-08-20 09:35:10 -0300

    feat: conclusão do primeiro modulo de next

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+    return (
+        <header>
+            <div>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <a href="#">home</a>
+                    <a href="#">posts</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
