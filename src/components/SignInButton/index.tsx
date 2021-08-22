235a4ca72f7c194c7e3ee336ccca0730216cda73
Andre Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2021-08-22 00:01:37 -0300

    criado o redirecionamento pra tela de pagamento

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,22 @@
 import { FaGithub } from 'react-icons/fa';
-import { FiX} from 'react-icons/fi';
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggerdIn = true;
-    
-    return isUserLoggerdIn ? (
-        <button type="button" className={styles.signInButton}>
-            <FaGithub color="#04d361" />
-            André Felipe
-            <FiX color="#737380" className={styles.closeIcon} />
-        </button>
-    ) : (
-        <button type="button" className={styles.signInButton}>
-            <FaGithub color="#eba417" />
-            Sign in with github
-        </button>
-    );
+  const [session] = useSession();
+
+  return session ? (
+    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
+      <FaGithub color="#04d361" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
+      <FaGithub color="#eba417" />
+      Sign in with github
+    </button>
+  );
 }
